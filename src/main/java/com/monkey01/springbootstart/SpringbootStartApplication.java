4146bffae4d75fa04097af6d518eb45edd7f54e0
kongsh8778 <48315749+kongsh8778@users.noreply.github.com>
2019-06-03 12:46:33 +0800

    Update SpringbootStartApplication.java

--- src/main/java/com/monkey01/springbootstart/SpringbootStartApplication.java
+++ src/main/java/com/monkey01/springbootstart/SpringbootStartApplication.java
@@ -3,9 +3,14 @@ package com.monkey01.springbootstart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootStartApplication {
+public class SpringbootStartApplication extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(AppLication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootStartApplication.class, args);
